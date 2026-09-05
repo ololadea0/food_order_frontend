d38cae3e986e838db065f94983fd55945c836172
ololadea0 <opeyemifawaz11@gmail.com>
2026-09-05 18:06:16 +0100

    Remove legacy frontend files

--- src/utils/formatCurrency.js
+++
@@ -1,8 +0,0 @@
-const nairaFormatter = new Intl.NumberFormat("en-NG", {
-  style: "currency",
-  currency: "NGN",
-  minimumFractionDigits: 2,
-});
-
-export const formatCurrency = (value = 0) =>
-  nairaFormatter.format(Number(value) || 0);
